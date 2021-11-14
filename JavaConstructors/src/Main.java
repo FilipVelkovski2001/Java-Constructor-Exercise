04c57363597b54c9fbf35f9dca164c77a30e6741
Filip <filipvelkovski12345@gmail.com>
2021-11-14 22:43:56 +0100

    Commit

---
+++ JavaConstructors/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Shirt s = new Shirt("White", 'M');
+		System.out.println(s.color);
+		System.out.println(s.size);
+		
+		
+		
+		
+		
+
+	}
+
+}
